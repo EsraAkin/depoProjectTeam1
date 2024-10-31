78bdb03641bc4db434c15320613ba442e221383e
hatice <haticeangilci@hotmail.com>
2024-10-31 12:59:03 +0300

    v5

--- src/main/java/ProductService.java
+++ src/main/java/ProductService.java
@@ -18,7 +18,7 @@ public class ProductService {
 
     //ÜRÜN TANIMLAMA   --Ertuğrul H.
     //ÜRÜN LİSTELEME   --Ahsen H.
-    //ÜRÜN GİRİŞİ      --Hatice H.
+    //ÜRÜN GİRİŞİ      --Hatice HHHHHHHHHHH
     //RAFA KOYMA       --Kerim H.
     //ÜRÜN ÇIKIŞI      --Yuşa H.
     //GÜNCELLEME       --ALPER h. eSRA
