1dd49895c7e881a17bab78a1a24d376e1c8d84d7
Ertuğrul Karaca <ek122302@gmail.com>
2024-10-31 19:48:15 +0300

    myFirstRepoTransfer

--- src/main/java/ProductService.java
+++ src/main/java/ProductService.java
@@ -1,28 +1,66 @@
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
 
 public class ProductService {
 
-    Scanner input=new Scanner(System.in);
+    Scanner input = new Scanner(System.in);
     String urunIsmi;
     String uretici;
     String birim;
-    int miktar;
+
     String raf;
 
-    Integer idCounter=1;
-
-    Map<Integer, Product> mevcutUrunler=new HashMap<>();
+    Integer idCounter = 1000;
+    int miktar = 0;
+    static Map<Integer, Product> mevcutUrunler = new HashMap<>();
 
 
     //ÜRÜN TANIMLAMA   --Ertuğrul H.
-    //ÜRÜN LİSTELEME   --Ahsen H.
-    //ÜRÜN GİRİŞİ      --Hatice HHHHHHHHHHH
-    //RAFA KOYMA       --Kerim H.
-    //ÜRÜN ÇIKIŞI      --Yuşa H.
-    //GÜNCELLEME       --ALPER h. eSRA eeee
-    //ARAMA            --Lambda
+    public void urunTanımlama() {
 
 
-}
+        Integer id = idCounter; // ID^yi otomatik arttırır.
+        idCounter++;
+
+        System.out.println("Lütfen Ürünün İsmini Giriniz");
+        String ürünİsmi = input.nextLine();
+
+        System.out.println("Lütfen Üretici İsmi Giriniz");
+        String üreticiİsmi = input.nextLine();
+
+        System.out.println("Lütfen Ürünün Birimini Giriniz");
+        String ürünBirimi = input.nextLine();
+
+        while (true) {
+            System.out.println("Lütfen Ürün Miktarını Giriniz:");
+            try {
+                miktar = input.nextInt();
+                input.nextLine();
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("Yanlış Giriş Yaptınız Sayı Girmelisiniz !");
+                input.nextLine();
+            }
+        }
+
+        System.out.println("Lütfen Raf İsmini Giriniz");
+        String rafİsim = input.nextLine();
+
+        Product yeniUrun = new Product(id, ürünİsmi, üreticiİsmi, miktar, ürünBirimi, rafİsim);
+        mevcutUrunler.put(id, yeniUrun);
+
+        System.out.println("Ürün başarıyla eklendi: ");
+
+
+        }
+        //ÜRÜN LİSTELEME   --Ahsen H.
+        //ÜRÜN GİRİŞİ      --Hatice HHHHHHHHHHH
+        //RAFA KOYMA       --Kerim H.
+        //ÜRÜN ÇIKIŞI      --Yuşa H.
+        //GÜNCELLEME       --ALPER h. eSRA eeee
+        //ARAMA            --Lambda
+
+
+    }
