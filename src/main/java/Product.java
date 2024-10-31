aed77fb441f1ecb23c82cd7318e41f45d3f99da3
hatice <haticeangilci@hotmail.com>
2024-10-31 12:41:34 +0300

    V3

--- src/main/java/Product.java
+++ src/main/java/Product.java
@@ -27,6 +27,7 @@ public class Product {
     public void setIdCounter(Integer idCounter) {
         this.idCounter = idCounter;
     }kakkakakmememememememememe
+    kakakakakakka
 
     public String getUrunIsmi() {
         return urunIsmi;
