f4dcc727754437e49479260fd86e97e5e9566d54
Esra AKINEDEN <92251077+EsraAkin@users.noreply.github.com>
2024-11-01 21:30:06 +0100

    Update Utils.java

--- src/main/java/Utils.java
+++ src/main/java/Utils.java
@@ -21,9 +21,9 @@ public class Utils {
             }
         }
     }
+    //---------------------------int yerine string exception metodu----------------------
 
-     //---------------------------Liste boş mu kontrolü----------------------
-
+    public class Utils1 {
         public static boolean urunKontrol(Map<Integer, Product> mevcutUrunler) {
             if (mevcutUrunler.isEmpty()) {
                 System.out.println("Depoda henüz tanımlanmış bir ürün yok! Lütfen önce bir ürün tanımlayın.");
@@ -31,5 +31,5 @@ public class Utils {
             }
             return true; // Depoda ürün varsa true döner
         }
-
+    }
 }
