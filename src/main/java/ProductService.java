2e5308909c5682244235226073b31f23e32f0ab2
Esra Akineden <e.akineden@gmail.com>
2024-11-01 18:45:28 +0100

    v5

--- src/main/java/ProductService.java
+++ src/main/java/ProductService.java
@@ -1,3 +1,13 @@
+
+//ÜRÜN TANIMLAMA   --Ertuğrul H.
+//ÜRÜN LİSTELEME   --Ahsen H.
+//ÜRÜN GİRİŞİ      --Hatice H.
+//RAFA KOYMA       --Kerim H.
+//ÜRÜN ÇIKIŞI      --Yuşa H.
+//GÜNCELLEME       --Esra H.
+//ARAMA            --Alper H. Lambda ile ürüne-üreticiye-kalan miktara göre arama yapılacak...
+
+
 import java.util.*;
 
 public class ProductService {
@@ -11,7 +21,7 @@ public class ProductService {
     int miktar = 0;
     Map<Integer, Product> mevcutUrunler = new HashMap<>();
 
-    //ÜRÜN TANIMLAMA   --Ertuğrul H.
+    //----------------------------ÜRÜN TANIMLAMA   --Ertuğrul H.----------------------------//
 
     public void urunTanimlama() {
 
@@ -43,12 +53,11 @@ public class ProductService {
             idCounter++;
 
         }
-
-
-        idCounter++;
-
         urunListele1();
     }
+
+
+    //----------------------------ÜRÜN GİRİŞİ   --Hatice H.----------------------------//
 
     public void urunGirisi() {
 
@@ -71,6 +80,8 @@ public class ProductService {
         urunListele1();
     }
 
+    //----------------------------ÜRÜN LİSTELE   --Ahsen H.----------------------------//
+
     public void urunListele1() {
 
         System.out.printf("%10s %10s %10s %10s %10s %10s%n", "ID", "İsim", "Üretici", "Miktar", "Birim", "Raf");
@@ -86,13 +97,7 @@ public class ProductService {
         }
     }
 
-    //ÜRÜN LİSTELEME   --Ahsen H.
-
-    //ÜRÜN GİRİŞİ      --Hatice Angılcı.
-    //RAFA KOYMA       --Kerim H.
-    //ÜRÜN ÇIKIŞI      --Yuşa H.
-    //GÜNCELLEME       --ALPER h. Esra AKINEDEN
-    //ARAMA            --Lambda ile ürüne-üreticiye-kalan miktara göre arama yapılacak...
+    //----------------------------ÜRÜN GÜNCELLE   --Esra H.----------------------------//
     public void urunuGuncelle() {
         if (!urunKontrol()) return; // Depoda ürün yoksa ana menüye dön
 
@@ -151,9 +156,116 @@ public class ProductService {
             System.out.println("Bu ID numarası ile bir ürün bulunamadı.");
         }
 
-        //urunListele(); // Güncellenmiş ürün listesini göster
+        urunListele1(); // Güncellenmiş ürün listesini göster
     }
 
+
+    public void urunRafakoyma() {
+
+        System.out.println("rafa koymak isteğiniz ürünün ıd sini giriniz :");
+        int id = input.nextInt();
+        input.nextLine();
+
+
+        if (mevcutUrunler.containsKey(id)) {
+            Product product = mevcutUrunler.get(id);
+            if (!product.getRaf().equals("null")) {
+                System.out.println("ürünün zaten raf numarası " + product.getRaf() + "noya  yerleştirilmiştir");
+
+            } else {
+
+                System.out.println("Lütfen raf numarasını giriniz(Sadece 100 ile 999 arasında sayı giriniz) :");
+                String yeniRaf12 = input.nextLine();
+
+                try {
+                    int yeniRaf=Integer.parseInt(yeniRaf12)  ;
+
+
+                    if (yeniRaf >= 100 && yeniRaf <= 900) {
+                        boolean rafDolu = mevcutUrunler.values().stream().anyMatch(mevcut -> mevcut.getRaf().equals(String.valueOf(yeniRaf)));
+
+
+                        if (!rafDolu) {
+                            product.setRaf(String.valueOf(yeniRaf));
+                            System.out.println("ürün başarıyla raf" + yeniRaf + "numarasına yerleştirilmiştir");
+
+                        } else {
+                            System.out.println("bu raf numarası başka bir ürün tarafından kullanılmaktadır");
+
+                        }
+                    } else {
+                        System.out.println("geçersiz raf numarası lütfen 100 ile 999 arasında bir sayı giriniz");
+                    }
+
+
+
+                }catch (NumberFormatException e){
+                    System.out.println("geçersiz raf numarası lütfen sayısal bir değer giriniz");
+                }
+            }
+        }else {
+            System.out.println("geçersiz ürün idsi ");
+
+
+        }
+        urunListele1();
+    }
+
+
+
+
+    //----------------------------ÜRÜN ARAMA   --Alper H.----------------------------//
+
+    public void urunArama() {
+        System.out.println("----Lutfen Arama Turunu Seciniz----");
+        System.out.println("1-Urun Ismi ile Arama");
+        System.out.println("2-Uretici Ismi ile Arama");
+        System.out.println("3-Miktar Altindaki Urunleri Arama");
+
+        int secim = input.nextInt();
+        input.nextLine();
+
+
+
+        switch (secim) {
+            case 1:
+                System.out.println("Lutfen Aranacak Urun Ismini Giriniz");
+                String aranacakIsim = input.nextLine();
+                System.out.printf("%-20s | %-20s | %-20s | %-10s | %-10s | %-10s%n",
+                        "Urun Numarasi", "Urun Ismi", "Uretici Ismi", "Miktar", "Birim", "Raf");
+                System.out.println("-".repeat(95));
+                mevcutUrunler.values().stream()
+                        .filter(t -> t.getUrunIsmi().equalsIgnoreCase(aranacakIsim))
+                        .forEach(System.out::println);
+                System.out.println("Aramanız tamamlandı. Teşekkür ederiz!");
+                break;
+
+            case 2:
+                System.out.println("Lutfen Aranacak Urun Ureticisini Giriniz");
+                String aranacakUretici = input.nextLine();
+                System.out.printf("%-20s | %-20s | %-20s | %-10s | %-10s | %-10s%n",
+                        "Urun Numarasi", "Urun Ismi", "Uretici Ismi", "Miktar", "Birim", "Raf");
+                System.out.println("-".repeat(95));
+                mevcutUrunler.values().stream().filter(t -> t.getUretici().equalsIgnoreCase(aranacakUretici)).forEach(System.out::println);
+                System.out.println("Aramanız tamamlandı. Teşekkür ederiz!");
+                break;
+
+            case 3:
+                System.out.println("Lutfen Miktar Sinirini Giriniz");
+                int miktarUstSinir = input.nextInt();
+                System.out.printf("%-20s | %-20s | %-20s | %-10s | %-10s | %-10s%n",
+                        "Urun Numarasi", "Urun Ismi", "Uretici Ismi", "Miktar", "Birim", "Raf");
+                System.out.println("-".repeat(95));
+                mevcutUrunler.values().stream().filter(t -> t.getMiktar() <= miktarUstSinir).forEach(System.out::println);
+                System.out.println("Aramanız tamamlandı. Teşekkür ederiz!");
+                break;
+
+            default:
+                System.out.println("Gecersiz Bir Secim Yaptiniz");
+                break;
+        }
+
+    }
 
     //----------------------------List boşsa yardımcı metodu-----------------------------
     private boolean urunKontrol() {
@@ -163,6 +275,7 @@ public class ProductService {
         }
         return true; // Depoda ürün varsa true döner
     }
+
 
 
 }
